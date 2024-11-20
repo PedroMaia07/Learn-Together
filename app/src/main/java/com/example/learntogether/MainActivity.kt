da5ed7bd2527465edab7900b6667de44c9f06d7b
Pedro <pedrovimaia@hotmail.com>
2024-11-20 17:48:59 -0300

    Tela Finalizada

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,61 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearnTogetherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                InitialScreen(stringResource(R.string.jetpack_compose_tutorial),
+                    stringResource(R.string.introduction_compose),
+                    stringResource(R.string.text_compose)
+                )
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun InitialScreen(
+    title:String,
+     introduction: String,
+     text:String,
+     modifier: Modifier = Modifier)
+{
+    Scaffold (
+        Modifier.fillMaxSize()
+    ){ innerPadding ->
+        Column(
+            modifier = modifier.padding(innerPadding),
+        ){
+            Image(
+                painter = painterResource(R.drawable.bg_compose_background),
+                contentDescription = null,
+            )
+            Text(
+                text = title,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+            Text(
+                text = introduction,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                textAlign = TextAlign.Justify,
+            )
+            Text(
+                text = text,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Justify,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        InitialScreen(stringResource(R.string.jetpack_compose_tutorial),
+            stringResource(R.string.introduction_compose),
+            stringResource(R.string.text_compose)
+        )
     }
 }
